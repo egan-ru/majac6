720af1a85ea69507eaaf1ed639c36fc558bff983
egan-ru <egan.fryazino@gmail.com>
2016-11-15 21:33:42 +0300

    soft uart: WIP

--- majac6.X/drivers/pic14/adctype0/adctype0.c
+++ majac6.X/drivers/pic14/adctype0/adctype0.c
@@ -1,3 +1,22 @@
+ /**
+    This file is simple adctype0 driver for pic14 (get time from radio majac)
+    majac6 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    simple adctype0 driver for pic14 is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with simple adctype0 driver for pic14.  If not, see <http://www.gnu.org/licenses/>.
+
+	topic on ru forum
+    http://www.microchip.su/showthread.php?t=17856&page=1
+**/
+
 #include "../../../core/hardware.h"
 
 /*pic12f675 have 0..3 channels*/
--- majac6.X/drivers/pic14/soft_uart/soft_uart.c
+++ majac6.X/drivers/pic14/soft_uart/soft_uart.c
@@ -1,49 +1,39 @@
  /**
-    This file is part of majac6 (get time from radio majac)
+    This file is simple soft uart driver for pic14 (get time from radio majac)
     majac6 is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    majac6 is distributed in the hope that it will be useful,
+    simple soft uart driver for pic14 is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with majac6.  If not, see <http://www.gnu.org/licenses/>.
+    along with simple soft uart driver for pic14.  If not, see <http://www.gnu.org/licenses/>.
 
 	topic on ru forum
     http://www.microchip.su/showthread.php?t=17856&page=1
-
 **/
 
-#include "core.h"
+#include "../../../core/hardware.h"
 
-void main(void){
-    boot_init();
-    wdt_init();
-    coretimer_init();
-    adctype0_init();
+#define OUT_PIN     GPIO0
+
+typedef enum{
+    idle = 0,
+    tx_req,
+    start,
+    data,
+    stop,
+    done,
+}soft_uart_state_t;
+
+void soft_uart_init(void){
     
-    
-    for(;;){
-        wdt_ctl();
-        /*user application there*/
-        
-        
-        
-    }
 }
 
-
-/*
- * hardware dependent
- * TODO:
- * 
- */
-void __interrupt generic_interrupt_handler(void){
-    if(CORETIMER_REQ){
-        coretimer_handler();
-    }
+void soft_uart_tx(uint8_t data){
+    
 }
